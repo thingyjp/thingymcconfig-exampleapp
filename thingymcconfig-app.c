76690a3fba4cd2bc387225ad85683816797ce7da
Daniel Palmer <daniel@0x0f.com>
2018-07-16 18:44:19 +0900

    Add oping library. Use oping to ping google and report connectivity status.

--- thingymcconfig-app.c
+++ thingymcconfig-app.c
@@ -1,10 +1,15 @@
 #include <glib.h>
 #include <gio/gunixsocketaddress.h>
 #include <thingymcconfig/ctrl.h>
+#include <oping.h>
+
+#define APPINDEX 1
+
+static pingobj_t* oping;
 
 static void sendappstate(GSocketConnection* socketconnection) {
 	struct thingymcconfig_ctrl_field_index appindex = { .type =
-	THINGYMCCONFIG_FIELDTYPE_APPSTATEUPDATE_APPINDEX, .index = 1 };
+	THINGYMCCONFIG_FIELDTYPE_APPSTATEUPDATE_APPINDEX, .index = APPINDEX };
 	struct thingymcconfig_ctrl_field_stateanderror appstate = { .type =
 	THINGYMCCONFIG_FIELDTYPE_APPSTATEUPDATE_APPSTATE, .state =
 	THINGYMCCONFIG_OK };
@@ -22,6 +27,28 @@ static void sendappstate(GSocketConnection* socketconnection) {
 	g_output_stream_flush(os, NULL, NULL);
 }
 
+static void sendconnectivitystate(GSocketConnection* socketconnection,
+		gboolean connected) {
+	struct thingymcconfig_ctrl_field_index appindex = { .type =
+	THINGYMCCONFIG_FIELDTYPE_APPSTATEUPDATE_APPINDEX, .index = APPINDEX };
+	struct thingymcconfig_ctrl_field_stateanderror connectivitystate = { .type =
+	THINGYMCCONFIG_FIELDTYPE_APPSTATEUPDATE_CONNECTIVITY, .state =
+			connected ? THINGYMCCONFIG_OK : THINGYMCCONFIG_ERR };
+
+	struct thingymcconfig_ctrl_msgheader msghdr = { .type =
+	THINGYMCCONFIG_MSGTYPE_EVENT_APPSTATEUPDATE, .numfields = 3 };
+
+	GOutputStream* os = g_io_stream_get_output_stream(
+			G_IO_STREAM(socketconnection));
+	g_output_stream_write(os, (void*) &msghdr, sizeof(msghdr), NULL, NULL);
+	g_output_stream_write(os, (void*) &appindex, sizeof(appindex), NULL, NULL);
+	g_output_stream_write(os, (void*) &connectivitystate,
+			sizeof(connectivitystate), NULL, NULL);
+	g_output_stream_write(os, (void*) &thingymcconfig_terminator,
+			sizeof(thingymcconfig_terminator), NULL, NULL);
+	g_output_stream_flush(os, NULL, NULL);
+}
+
 static gboolean ctrlsockcallback(GIOChannel *source, GIOCondition cond,
 		gpointer data) {
 	GSocketConnection* socketconnection = data;
@@ -30,7 +57,9 @@ static gboolean ctrlsockcallback(GIOChannel *source, GIOCondition cond,
 			G_IO_STREAM(socketconnection));
 
 	struct thingymcconfig_ctrl_msgheader msghdr;
-	g_input_stream_read(is, &msghdr, sizeof(msghdr), NULL, NULL);
+	if (g_input_stream_read(is, &msghdr, sizeof(msghdr), NULL, NULL)
+			!= sizeof(msghdr))
+		goto err;
 
 	g_message("have message type %d with %d fields", (int) msghdr.type,
 			(int) msghdr.numfields);
@@ -57,13 +86,35 @@ static gboolean ctrlsockcallback(GIOChannel *source, GIOCondition cond,
 	if (!terminated)
 		g_message("didn't see terminator");
 
+	return TRUE;
+	err: //
 	return FALSE;
 }
 
+static gboolean checkconnectivity(gpointer user_data) {
+	GSocketConnection* connection = user_data;
+	ping_send(oping);
+	pingobj_iter_t *iter;
+	gboolean reachable = FALSE;
+	for (iter = ping_iterator_get(oping); iter != NULL; iter =
+			ping_iterator_next(iter)) {
+		double latency;
+		size_t len = sizeof(latency);
+		ping_iterator_get_info(iter, PING_INFO_LATENCY, &latency, &len);
+		g_message("latency %f", latency);
+		if (latency > 0)
+			reachable = TRUE;
+	}
+	sendconnectivitystate(connection, reachable);
+	return G_SOURCE_CONTINUE;
+}
+
 int main(int argc, char** argv) {
+	oping = ping_construct();
+	ping_host_add(oping, "google.com");
 
 	GSocketAddress* socketaddress = g_unix_socket_address_new(
-			"/tmp/thingymcconfig.socket");
+			THINGYMCCONFIG_CTRLSOCKPATH);
 
 	GSocket* sock = g_socket_new(G_SOCKET_FAMILY_UNIX, G_SOCKET_TYPE_STREAM,
 			G_SOCKET_PROTOCOL_DEFAULT, NULL);
@@ -89,6 +140,9 @@ int main(int argc, char** argv) {
 	sendappstate(socketconnection);
 
 	GMainLoop* mainloop = g_main_loop_new(NULL, FALSE);
+
+	g_timeout_add(60 * 1000, checkconnectivity, socketconnection);
+
 	g_main_loop_run(mainloop);
 
 	return 0;
